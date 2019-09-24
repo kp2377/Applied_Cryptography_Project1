366242fa02a13b8e48db5813aa79be3e8ccba005
deepraj <sonideepraj@gmail.com>
2019-09-24 17:19:38 -0400

    Cipher first draft completed.

--- Encryption.cpp
+++ Encryption.cpp
@@ -18,6 +18,24 @@
 
 using namespace std;
 
+void cipher(char * pt, int length, int **key, int *ct);
+
+void print_key(int **key) {
+    int count = 0, count2 = 96;
+    for(int j=0;j<513;j++)
+    {
+	cout.width(3);
+        cout<< key[j/KEY_COLUMN][j%KEY_COLUMN]<<" ";
+	if(key[j/KEY_COLUMN][j%KEY_COLUMN])
+	    count++;
+	if(j%KEY_COLUMN == KEY_COLUMN - 1) {
+	    cout << " = " << (char)(count2++) << " " << count << "\n";
+	    count = 0;
+	}
+    }
+}
+
+
 int random_generator()  // random generator function that excludes 76 to 94
 {
     int rg;
@@ -30,15 +48,40 @@ int random_generator()  // random generator function that excludes 76 to 94
     return rg;
 }
 
+void cipher(char *pt, int length, int **key, int *ct) {
+    int alphabet_weight[27] = {19,7,1,2,4,10,2,2,5,6,1,1,3,2,6,6,2,1,5,5,7,2,1,2,1,2,1};
+    cout<<"The key in Cipher : \n";
+    print_key(key);
+    cout << "pt = " << pt <<"\n";
+    unsigned char pt_c;
+    int count,i;
+    int key_list;
+    for(i=0;i<length;i++) {
+	pt_c = pt[i];
+	if(pt_c < 96)
+	    key_list= 0;
+	else
+	    key_list = pt_c - 96;
+      //cout << key_list << " ";	
+	ct[i] = key[key_list][rand()%alphabet_weight[key_list]];
+	cout << ct[i] << ",";
+
+    }
+
+}
+
 
 int main()
 {
     int i;
     int count;
+    int length = 500;
     int random_array[106] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105};
     
     int alphabet_weight[27] = {19,7,1,2,4,10,2,2,5,6,1,1,3,2,6,6,2,1,5,5,7,2,1,2,1,2,1};
-    int same_weight[20] =     { 0,7,8,1,1,3,3,2,0,0,1,0,0,0,0,0,0,0,0,1}
+    int same_weight[20] =     { 0,7,8,1,1,3,3,2,0,0,1,0,0,0,0,0,0,0,0,1};
+    int ciphertext[500];
+    char plaintext[5][501] = {"so delightful up dissimilar by unreserved it connection frequently do an high room so in paid up on cousin ye dinner should in sex stood tried walls manor truth shy and three his their to years so child truth honoured peculiar families sensible up likewise by on in compliment interested discretion estimating on stimulated apartments oh dear so sing when in find read of call as distrusts behaviour abilities defective is never at water me might on formed merits hunted unable merely by mr whence or", "Had strictly mrs handsome mistaken cheerful We it so if resolution invitation remarkably unpleasant conviction As into ye then form To easy five less if rose were Now set offended own out required entirely Especially occasional mrs discovered too say thoroughly impossible boisterous My head when real no he high rich at with After so power of young as Bore year does has get long fat cold saw neat Put boy carried chiefly shy general And sir dare view but over man So at within mr to simple assure M", "Had strictly mrs handsome mistaken cheerful We it so if resolution invitation remarkably unpleasant conviction As into ye then form To easy five less if rose were Now set offended own out required entirely Especially occasional mrs discovered too say thoroughly impossible boisterous My head when real no he high rich at with After so power of young as Bore year does has get long fat cold saw neat Put boy carried chiefly shy general And sir dare view but over man So at within mr to simple assure M", "Had strictly mrs handsome mistaken cheerful We it so if resolution invitation remarkably unpleasant conviction As into ye then form To easy five less if rose were Now set offended own out required entirely Especially occasional mrs discovered too say thoroughly impossible boisterous My head when real no he high rich at with After so power of young as Bore year does has get long fat cold saw neat Put boy carried chiefly shy general And sir dare view but over man So at within mr to simple assure M", "Had strictly mrs handsome mistaken cheerful We it so if resolution invitation remarkably unpleasant conviction As into ye then form To easy five less if rose were Now set offended own out required entirely Especially occasional mrs discovered too say thoroughly impossible boisterous My head when real no he high rich at with After so power of young as Bore year does has get long fat cold saw neat Put boy carried chiefly shy general And sir dare view but over man So at within mr to simple assure M"};
     
     //int *key = (int *)calloc(KEY_ROW*KEY_COLUMN, sizeof(int));
     int** key = new int*[KEY_ROW];
@@ -58,7 +101,7 @@ int main()
         cout<<alphabet_weight[i]<<'\n';
     }
     
-    srand (time(NULL));
+    //srand (time(NULL));
     
     int random_number;
     int temp1 = 0;
@@ -118,18 +161,9 @@ int main()
     }
 
     cout<<"The key initialized is : \n";
-    count = 0;
-    for(int j=0;j<513;j++)
-    {
-	cout.width(3);
-        cout<< key[j/KEY_COLUMN][j%KEY_COLUMN]<<" ";
-	if(key[j/KEY_COLUMN][j%KEY_COLUMN])
-	    count++;
-	if(j%KEY_COLUMN == KEY_COLUMN - 1) {
-	    cout << " = " << count << "\n";
-	    count = 0;
-	}
-    }
+    print_key(key);
+
+    cipher(plaintext[0], length, key, ciphertext);
 
     return 0;
 }
