5e1c7b603fb3a6008d238f6878590e70de96a877
kartikey purohit <kp2377@nyu.edu>
2019-10-13 19:57:39 -0400

    Hi this is optimized codegit add .

--- test2.cpp
+++ test2.cpp
@@ -6,6 +6,8 @@
 
 using namespace std;
 
+ long int alphabet_array[27] = {0,63543,119315,220001,285496,330315,372427,403862,439973,488263,496900,504852,533867,589670,614784,644477,727844,733139,800065,912947,963557,999050,1015833,1039738,1039738,1042474};
+
 
 class Node  
 {  
@@ -57,25 +59,28 @@ int main()
   FILE *dictionary;
   long int n =0;
   long int total_lines=0, flag = 0,i = 0, k =0;
-  char ct = ' ';
+  char ct = '`';
   char *line = NULL;
   size_t len = 0;
   long int offset = 2;
+  int counter=0;
+
   //ssize_t nread;
-  dictionary = fopen("english_words.txt", "r");
-  string search = "chron";
+  string search = "bana";
   string copied;
-  long int alphabet_array[27];
-  long int temp=0, nread;
+   long int temp=0, nread;
+
+////////////////////////////////////////////////////////////////
   
   //Initializing linked List
   Node* head = NULL;
   Node* second = NULL;
   Node* base = NULL;
-  ///
+  
   head = new Node();
   second = new Node();
   base = new Node();
+
   //connecting head and base
   head->data = "Kartikey";
   head->next = second;
@@ -86,82 +91,57 @@ int main()
   base->data= "Deepraj";
   base->next = NULL;
 
+
+/////////////////////////////////////////////////////////////////  
+  for(int m=0;search[m]!='\0';m++)
+  {
+    counter++;
+  }
+  dictionary = fopen("english_words.txt", "r");
   if(dictionary == NULL)
   {
      perror ("Error opening file.");
   }
-  else
-  { 
-    start = clock();
-    while((nread = getline(&line, &len, dictionary)) != -1)
-    {
-	//cout<<"The nread value is: "<<nread<<"\n";
-        temp = temp + nread;
-        //printf("length = %ld: \n total length = %ld\n\n\n", nread, temp);
-        if(ct != line[0])
-        {
-		flag = 1;
-		alphabet_array[i++] = temp;
-        }
-	ct = line[0];
-        if(flag == 1)
-	{
-	   cout<<"The number of elements of "<<(char)((int)ct-1)<<" are: "<<alphabet_array[i-1]<<"\n";
-	   flag = 0;
-	}
-        //cout<<len<i<"\n";
-        ++total_lines;
 
 
-     int counter=0;
-     for(int m=0;search[m]!='\0';m++)
+  start = clock();
+  int initial_letter = search[0];
+  int value = (int)search[0]-97;
+  nread = 0;
+  long int jump = alphabet_array[value];
+  fseek(dictionary,jump,SEEK_SET);
+  nread = getline(&line, &len, dictionary);
+  temp=0;
+  temp = temp+nread;
+  while(line[0] == initial_letter)
+  {
+    i=0;
+    int flg = 1;
+    int diff = 0, clt=0;
+    for(int j=0;j<nread;j++)
+    { 
+     clt = (j-i);
+     if(search[i] == line[j])
      {
-	counter++;
-	//cout<<search[m]<<","<<counter<<"\n";
-     }
-     
-     //cout<<line<<"\n";
-       i=0;
-       int flg = 1;
-       int diff = 0, clt=0;
-       for(int j=0;j<nread;j++)
-       { 
-	 clt = (j-i);
-      	 if(search[i] == line[j] && search[0] == line[0])
-	 {
-	    if(diff != clt){i=0;}
-	    diff = clt;
-            if(flg)i++;
-       	    //cout<<counter<<" and "<<i<<"\n";
-            if(counter == i)
-     	    {
-	      flg = 0;
-	      cout<<"found!!\n";
-              copied = line;
-	      append(head, copied);
-	      i = 0;
-            }
-	 } 
+       if(diff != clt){i=0;}
+       diff = clt;
+       if(flg)i++;
+       if(counter == i)
+       {
+         flg = 0;
+         copied = line;
+	 append(head, copied);
+	 i = 0;
        }
-
-     }   
-    //cout<<"The total number of line: "<<total_lines<<'\n';
-
-    if (feof(dictionary))
-    {
-      puts ("End-of-File reached.");
-      printf ("Total number of bytes read: %ld\n", n);
-      printf ("Total number of lines read: %ld\n", total_lines);
-    }
-   else
-   { 
-     puts("End-of-File was not reached.");
-   }
-     free(line);
-     fclose(dictionary);
+      } 
+     }
+    fseek(dictionary,jump+temp,SEEK_SET);
+    nread = getline(&line, &len, dictionary);
+    temp = temp+nread; 
   }
+  free(line); 
+  fclose(dictionary);
   end = clock();
-  //float seconds = ((float)end-(float)start_n)/CLOCKS_PER_SEC;
   cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
   cout<<"Second is : "<<cpu_time_used<<"\n";
   printList(head);
